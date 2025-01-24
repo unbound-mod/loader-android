b03e541f6fd0dba2baa20bc47ce0e1fa74d651b7
Mario P. <me@marioparaschiv.com>
2025-01-24 14:15:16 +0000

    ✨ feat: shortened modules script

--- app/src/main/assets/js/modules.js
+++ app/src/main/assets/js/modules.js
@@ -1,12 +1,14 @@
 Object.defineProperties(globalThis, {
 	__d: {
 		configurable: true,
+
 		get() {
-			if (!globalThis.modules) {
-				globalThis.modules = globalThis.__c?.();
-			}
+			globalThis.modules ??= globalThis.__c?.();
+
+
 			return this.value;
 		},
+
 		set(v) {
 			this.value = v;
 		}
